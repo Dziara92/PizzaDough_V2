16b04a995e022ab92dadeff1096c6070245d1b5a
Mateusz Dziura <mateusz.dziura92@gmail.com>
2024-05-07 19:09:27 +0200

    create slider add images and functionality

--- src/page/mainPage.jsx
+++ src/page/mainPage.jsx
@@ -1,16 +1,35 @@
-import { ArrowNext, ArrowPrev } from "../assets/arrows";
+import Slider from "../components/slider";
 
 const MainPage = () => {
+  const slides = [
+    {
+      id: 1,
+      heading: "Kalkulator składników",
+      text: "Jeśli masz problem z odmierzeniem dokładnych proporcji nie ma problemu, mamy dla Ciebie kalkulator składników!",
+      txtBtn: "Kalkulator",
+      img: "../../public/Pizza_1.jpg",
+      url: "/kalkulator",
+    },
+    {
+      id: 2,
+      heading: "Sklep",
+      text: "Potrzebujesz składników, a może pieca, który wpiecze Twoja idealna pizze? Zapraszamy do naszego sklepu.",
+      txtBtn: "Sklep",
+      img: "../../public/Pizza_2.jpg",
+      url: "/sklep",
+    },
+    {
+      id: 3,
+      heading: "Blog",
+      text: "Lorem ipsum",
+      txtBtn: "Lorem",
+      img: "../../public/Pizza_3.jpg",
+      url: "/",
+    },
+  ];
   return (
     <div className="container mx-auto my-9 px-[5px] w-4/5 md:mt-20 ">
-      <div className=" w-full h-96 bg-black relative">
-        <button className=" absolute top-2/4 left-5 -translate-y-1/2 text-white w-   ">
-          <ArrowPrev prop={"w-9 h-9"} />
-        </button>
-        <button className=" absolute top-2/4 right-5 -translate-y-1/2 text-white ">
-          <ArrowNext prop={"w-9 h-9"} />
-        </button>
-      </div>
+      <Slider slides={slides} />
     </div>
   );
 };
