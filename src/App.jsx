e6df077791c5d4ea88b5d98e2f34b278946fa281
Mateusz Dziura <mateusz.dziura92@gmail.com>
2024-05-09 12:29:09 +0200

    fixed css in contatctPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ShopPage from "./page/shopPage";
 import CalculatorPage from "./page/calculatorPage";
 import SingleProductPage from "./page/singleProductPage";
 import BasketPage from "./page/basketPage";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="koszyk" element={<BasketPage />} />
         <Route path="kontakt" element={<ContactPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/page/contactPage.jsx
+++ src/page/contactPage.jsx
@@ -1,34 +1,6 @@
-import { useState } from "react";
+import ContactForm from "../components/contactForm";
 
 const ContactPage = () => {
-  const [checkNum, setCheckNum] = useState(false);
-  const [dataForm, setDataForm] = useState({
-    name: "",
-    email: "",
-    tel: "",
-    message: "",
-  });
-
-  const handleFormChange = (e) => {
-    const { name, value } = e.target;
-
-    if (name === "tel" && !/^\d+$/.test(value)) {
-      return;
-    }
-
-    setDataForm({ ...dataForm, [name]: value });
-  };
-
-  const formSubmit = (e) => {
-    e.preventDefault();
-    setDataForm({
-      name: "",
-      email: "",
-      tel: "",
-      message: "",
-    });
-  };
-
   return (
     <div className="container mx-auto my-9 px-[5px] w-4/5">
       <h1 className=" animate-slideDown text-center mb-8 text-3xl font-semibold md:text-5xl md:mb-16">
@@ -43,42 +15,7 @@ const ContactPage = () => {
           <p>7/24</p>
         </div>
         <div className=" mt-8 flex flex-col md:mt-0 lg:w-1/2">
-          <form onSubmit={formSubmit} className="flex flex-col">
-            <input
-              type="text"
-              placeholder="Imię i Nazwisko"
-              className=" mb-4 p-2 text-sm outline-none text-gray-700 border-2"
-              name="name"
-              value={dataForm.name}
-              onChange={handleFormChange}
-            />
-            <input
-              type="email"
-              placeholder="E-mail"
-              className=" mb-4 p-2 text-sm outline-none text-gray-700 border-2"
-              name="email"
-              value={dataForm.email}
-              onChange={handleFormChange}
-            />
-            <input
-              type="text"
-              placeholder="Telefon"
-              className=" mb-4  p-2 text-sm outline-none text-gray-700 border-2"
-              name="tel"
-              value={dataForm.tel}
-              onChange={handleFormChange}
-            />
-            <textarea
-              name="message"
-              placeholder="Wiadomość"
-              className=" p-2 text-sm outline-none text-gray-700 border-2"
-              value={dataForm.message}
-              onChange={handleFormChange}
-            ></textarea>
-            <button className=" my-6 p-2 uppercase bg-buttonBgc text-white text-sm lg:text-base lg:w-1/2 xl:w-[45%]">
-              Wyślij
-            </button>
-          </form>
+          <ContactForm />
           <p className=" text-xs text-slate-600 text-center md:text-left">
             Administratorem danych osobowych podanych w formularzu jest
             PizzaDough Company, via Padole 218, Vincenza. Zasady przetwarzania
